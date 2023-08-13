5e8380b6644982e3091ced8568deff20925f8cc4
Haly Yampolska <halyna.yampolska@gmail.com>
2023-08-13 19:15:09 +0300

    Add dinamic slider on the main page

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery'), 'version' => '209b24266e1c066c34c6');
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import "../css/style.scss"
+
+// Our modules / classes
+import MobileMenu from "./modules/MobileMenu"
+import HeroSlider from "./modules/HeroSlider"
+import GoogleMap from "./modules/GoogleMap.js"
+import UniversitySearch from "./modules/universitySearch"
+import MyNotes from "./modules/MyNotes"
+import Like from "./modules/Like"
+
+
+// Instantiate a new object using our modules/classes
+const mobileMenu = new MobileMenu()
+const heroSlider = new HeroSlider()
+const goolgeMap = new GoogleMap()
+const universitySearch = new UniversitySearch()
+const myNotes = new MyNotes()
+const like = new Like()
+
